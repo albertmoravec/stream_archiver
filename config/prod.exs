8f3b52350fabb231aabdbc389f9cdf9d0f80bc78
Albert Moravec <albert.moravec@gmail.com>
2022-08-29 20:12:13 +0200

    Fix pipeline definition

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,5 @@
 import Config
 
-config :stream_archiver_api_web, StreamArchiverApiWeb.Endpoint
-
 config :stream_archiver_web, StreamArchiverWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
