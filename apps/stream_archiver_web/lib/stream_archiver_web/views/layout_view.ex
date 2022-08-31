3b54e0f9f304d544cb16833fbad0013a34f2d72c
Albert Moravec <albert.moravec@gmail.com>
2022-08-31 17:49:48 +0200

    Adjust sidebar menu

--- apps/stream_archiver_web/lib/stream_archiver_web/views/layout_view.ex
+++ apps/stream_archiver_web/lib/stream_archiver_web/views/layout_view.ex
@@ -4,4 +4,14 @@ defmodule StreamArchiverWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def gravatar_hash(email) do
+    hash_input =
+      email
+      |> String.trim()
+      |> String.downcase()
+
+    :crypto.hash(:md5, hash_input)
+    |> Base.encode16(case: :lower)
+  end
 end
